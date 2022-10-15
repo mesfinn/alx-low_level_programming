12a1c939ef32813a535c7bce6aacfdc17881adfa
mesfinn <mesfinnuru@gmail.com>
2022-10-15 12:44:55 -0700

    printf

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,11 @@
 #include<stdio.h>
-int main(void)
-{
-	char word[] = "Programming is like building a multilingual puzzle";
 
-	puts(word);
+int main(void)
+
+{
+	char Mystr[] = "Programming is like building a multilingual puzzle";
+
+	puts(Mystr);
 
 	return (0);
 };
