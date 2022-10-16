85e55d2c64bf56ac8f218bf6cb6cefc690f74930
mesfinn <mesfinnuru@gmail.com>
2022-10-16 02:29:09 -0700

    prit_size

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Sucess)
+ */
+
+int main(void)
+{
+	char c;
+	int i;
+	long int a;
+	long long int b;
+	float f;
+
+	printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(c));
+	printf("Size of a int: %lu byte(s)\n", (unsigned long)sizeof(i));
+	printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(a));
+	printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof
+		(b));
+	printf("Size of a float: %lu byte(s)\n,", (unsigned long)sizeof(f));
+
+	return (0);
+};
