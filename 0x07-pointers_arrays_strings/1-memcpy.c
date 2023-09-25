f8adbadf7ea4b7b6feff0729516f1206c97f8979
mesfinn <mesfinnuru@gmail.com>
2023-09-25 12:40:30 -0700

    memcpy

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -10,11 +10,11 @@
  */
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
-	unsigned int i;
+	char *p = dest;
 
-	for (i = 0; i < n; i++)
+	while (n--)
 	{
-		src[i] = dest[i];
+		*p++ = *src++;
 	}
 	return (dest);
 }
